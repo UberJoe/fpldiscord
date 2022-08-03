a5becfe1c9815c9e34cc22cdae641fc7103a96b6
Joe Webster <joe.webster@islonline.com>
2022-08-03 21:10:44 +0100

    added descriptions for commands

--- draft/bot.py
+++ draft/bot.py
@@ -4,7 +4,7 @@ import os
 from discord.ext import commands
 import pandas as pd
 # import config as cnf # uncomment if in dev env
-# bot = discord.Bot(debug_guilds=[1001437221640994836, 1000070235954610177])
+# bot = discord.Bot(debug_guilds=[1001437221640994836])
 
 description = 'Bot for Coq Au Ian'
 intents = discord.Intents.all()
@@ -14,7 +14,7 @@ u = Utils()
 
 fixtures = bot.create_group("fixtures", "retrieve fixtures")
 
-@bot.command()
+@bot.command(description="Finds the owner for the specified player")
 async def owner(ctx, *, player_name: discord.Option(str)):
     player_df = u.get_player_attr(player_name, "team_x")
 
@@ -30,7 +30,7 @@ async def owner(ctx, *, player_name: discord.Option(str)):
             response += row.first_name + ' ' + row.second_name + ' is a free agent!\n'
     await ctx.respond(response)
 
-@fixtures.command()
+@fixtures.command(description="Responds with the H2H fixtures for the coming GW")
 async def this_week(ctx):
     matches_df = u.get_fixtures()
     gw_df = matches_df.loc[matches_df['match'] == u.current_gw()]
@@ -40,7 +40,7 @@ async def this_week(ctx):
         response += row.home_player + " vs " + row.away_player + "\n"
     await ctx.respond(response)
 
-@fixtures.command(name='gw')
+@fixtures.command(name='gw', description="Responds with the H2H fixtures for the specified GW")
 async def gw_subcommand(ctx, gameweek: discord.Option(int)):
     matches_df = u.get_fixtures()
     gw_df = matches_df.loc[matches_df['match'] == gameweek]
@@ -50,7 +50,7 @@ async def gw_subcommand(ctx, gameweek: discord.Option(int)):
         response += row.home_player + " vs " + row.away_player + "\n"
     await ctx.respond(response)
 
-@fixtures.command(name='team')
+@fixtures.command(name='team', description="Responds with the next 5 H2H fixtures for the specified team")
 async def team_subcommand(ctx, team: discord.Option(str)):
     matches_df = u.get_fixtures()
     next5_df = matches_df.loc[matches_df['match'] < (u.current_gw() + 5)]
@@ -61,7 +61,7 @@ async def team_subcommand(ctx, team: discord.Option(str)):
         response += row.home_player + " vs " + row.away_player + "\n"
     await ctx.respond(response)
 
-@bot.command()
+@bot.command(description="Responds with a message for whenever Dave pipes up")
 async def dave(ctx):
     await ctx.respond("fuck you Dave")
 
