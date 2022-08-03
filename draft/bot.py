403f5ddff39fa09b6ca247c341e390016648fadf
Joe Webster <joe.webster@islonline.com>
2022-08-03 07:29:38 +0100

    made data retrieval more dynamic

--- draft/bot.py
+++ draft/bot.py
@@ -1,23 +1,18 @@
+from draft.Utils import Utils
 import discord
 import os
 from discord.ext import commands
-from draft import utils
 import pandas as pd
+# import config as cnf # uncomment if in dev env
 
 description = 'Bot for Coq Au Ian'
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', description=description, intents=intents)
-
-# import config as cnf # uncomment if in dev env
-
-@bot.command()
-async def refresh_data(ctx):
-    utils.get_json()
-    await ctx.send("Done, I hope")
+u = Utils()
 
 @bot.command()
 async def owner(ctx, *, player: str):
-    players_df = utils.get_team_players()
+    players_df = u.get_team_players()
     
     player_df = players_df.loc[players_df['web_name'].str.lower() == player.lower()]
     if player_df.empty:
@@ -32,15 +27,15 @@ async def owner(ctx, *, player: str):
 
 @bot.group(name='fixtures', invoke_without_command=True)
 async def fixtures(ctx):
-    matches_df = utils.get_fixtures()
-    gw_df = matches_df.loc[matches_df['match'] == utils.current_gw()]
+    matches_df = u.get_fixtures()
+    gw_df = matches_df.loc[matches_df['match'] == u.current_gw()]
 
     for row in gw_df.itertuples():
         await ctx.send(row.home_player + " vs " + row.away_player)
 
 @fixtures.command(name='gw')
 async def gw_subcommand(ctx, week: int):
-    matches_df = utils.get_fixtures()
+    matches_df = u.get_fixtures()
     gw_df = matches_df.loc[matches_df['match'] == week]
 
     for row in gw_df.itertuples():
@@ -48,8 +43,8 @@ async def gw_subcommand(ctx, week: int):
 
 @fixtures.command(name='team')
 async def team_subcommand(ctx, team: str):
-    matches_df = utils.get_fixtures()
-    next5_df = matches_df.loc[matches_df['match'] < (utils.current_gw() + 5)]
+    matches_df = u.get_fixtures()
+    next5_df = matches_df.loc[matches_df['match'] < (u.current_gw() + 5)]
     team_df = next5_df.loc[(next5_df['home_player'].str.lower() == team.lower()) | (next5_df['away_player'].str.lower() == team.lower())]
 
     for row in team_df.itertuples():
--- draft/utils.py
+++ draft/utils.py
@@ -4,203 +4,172 @@ import json
 import pandas as pd
 import math
 
-def get_json():
-    """
-    Pulls fpl draft league data using an api call, and stores the output
-    in a json file at the specified location.
-    
-    To get the FPL Draft api call, I followed advice on [Reddit here](https://www.reddit.com/r/FantasyPL/comments/9rclpj/python_for_fantasy_football_using_the_fpl_api/e8g6ur4?utm_source=share&utm_medium=web2x) which basically said you can derive the API calls by using Chrome's developer window under network you can see the "fetches" that are made, and the example response data. Very cool!
-    
-    :param file_path: The file path and name of the json file you wish to create
-    :param api: The api call for your fpl draft league
-    :param email_address: Your email address to authenticate with premierleague.com
-    :returns: 
-    """
-    json_files = [
-        os.path.realpath("")+'/data/transactions.json',
-        os.path.realpath("")+'/data/elements.json',
-        os.path.realpath("")+'/data/details.json',
-        os.path.realpath("")+'/data/element_status.json'
-    ]
-    
-    apis = [
-        'https://draft.premierleague.com/api/draft/league/36298/transactions',
-        'https://draft.premierleague.com/api/bootstrap-static',
-        'https://draft.premierleague.com/api/league/36298/details',
-        'https://draft.premierleague.com/api/league/36298/element-status'
-    ]
-    
-    # Post credentials for authentication
-    session = requests.session()
-    url = 'https://users.premierleague.com/accounts/login/'
-    payload = {
-     'password': os.environ['pwd'],
-     'login': os.environ['email'],
-     'redirect_uri': 'https://fantasy.premierleague.com/a/login',
-     'app': 'plfpl-web'
+class Utils:
+
+    api = {
+        'transactions': 'https://draft.premierleague.com/api/draft/league/36298/transactions',
+        'elements': 'https://draft.premierleague.com/api/bootstrap-static',
+        'details': 'https://draft.premierleague.com/api/league/36298/details',
+        'element_status': 'https://draft.premierleague.com/api/league/36298/element-status'
     }
-    session.post(url, data=payload)
-    
-    # Loop over the api(s), call them and capture the response(s)
-    for file, i in zip(json_files, apis):
-        r = session.get(i)
-        jsonResponse = r.json()
-        with open(file, 'w') as outfile:
-            json.dump(jsonResponse, outfile)
 
-def get_data(df_name):
-    # Dataframes from the details.json
-    if df_name == 'league_entries':
-        with open(os.path.realpath("")+'/data/details.json') as json_data:
-            d = json.load(json_data)
-            league_entry_df = pd.json_normalize(d['league_entries'])
-            
-        return league_entry_df
-    
-    elif df_name == 'matches':
-        with open(os.path.realpath("")+'/data/details.json') as json_data:
-            d = json.load(json_data)
-            matches_df = pd.json_normalize(d['matches'])
-            
+    def __init__(self):
+        self.session = requests.session()
+        url = 'https://users.premierleague.com/accounts/login/'
+        payload = {
+        'password': os.environ['pwd'],
+        'login': os.environ['email'],
+        'redirect_uri': 'https://fantasy.premierleague.com/a/login',
+        'app': 'plfpl-web'
+        }
+        self.session.post(url, data=payload)
+
+    def get_data(self, df_name):
+        # Dataframes from the details.json
+        if df_name == 'league_entries':
+            r = self.session.get(self.api['details']).json()
+            league_entry_df = pd.json_normalize(r['league_entries'])
+                
+            return league_entry_df
+        
+        elif df_name == 'matches':
+            r = self.session.get(self.api['details']).json()
+            matches_df = pd.json_normalize(r['matches'])
+                
+            return matches_df
+        
+        elif df_name == 'standings':
+            r = self.session.get(self.api['details']).json()
+            standings_df = pd.json_normalize(r['standings'])
+                
+            return standings_df
+        
+        # Dataframes from the elements.json
+        elif df_name == 'elements':
+            r = self.session.get(self.api['elements']).json()
+            elements_df = pd.json_normalize(r['elements'])
+                
+            return elements_df
+        
+        elif df_name == 'element_types':
+            r = self.session.get(self.api['elements']).json()
+            element_types_df = pd.json_normalize(r['element_types'])
+                
+            return element_types_df
+        
+        # Dataframes from the transactions.json
+        elif df_name == 'transactions':
+            r = self.session.get(self.api['transactions']).json()
+            transactions_df = pd.json_normalize(r['transactions'])
+                
+            return transactions_df
+        
+        # Dataframes from the element_status.json
+        elif df_name == 'element_status':
+            r = self.session.get(self.api['element_status']).json()
+            element_status_df = pd.json_normalize(r['element_status'])
+                
+            return element_status_df
+
+    def get_team_players(self):
+        
+        # Pull the required dataframes
+        element_status_df = self.get_data('element_status')
+        elements_df = self.get_data('elements')
+        element_types_df = self.get_data('element_types')
+        league_entry_df = self.get_data('league_entries')
+        standings_df = self.get_data('standings')
+        
+        # Built the initial player -> team dataframe
+        players_df = (pd.merge(element_status_df,
+                            league_entry_df,
+                            how="outer",
+                            left_on='owner',
+                            right_on='entry_id'
+                            )
+                .drop(columns=['in_accepted_trade',
+                                'owner',
+                                'status',
+                                'entry_id',
+                                'entry_name',
+                                'id',
+                                'joined_time',
+                                'player_last_name',
+                                'short_name',
+                                'waiver_pick'])
+                .rename(columns={'player_first_name':'team'})
+                )
+        
+        # Get the element details
+        players_df = pd.merge(players_df, elements_df, how="outer", left_on='element', right_on='id')
+        players_df = players_df[['team_x',
+                                'element',
+                                'web_name',
+                                'first_name',
+                                'second_name',
+                                'total_points',
+                                'goals_scored',
+                                'goals_conceded',
+                                'clean_sheets',
+                                'assists',
+                                'bonus',
+                                'draft_rank',
+                                'element_type',
+                                'points_per_game',
+                                'red_cards',
+                                'yellow_cards'
+                                ]]
+        
+        # Get the player types (GK, FWD etc.)
+        players_df = (pd.merge(players_df,
+                            element_types_df,
+                            how='outer',
+                            left_on='element_type',
+                            right_on='id')
+                    .drop(columns=['id',
+                                    'plural_name_short',
+                                    'singular_name',
+                                    'singular_name_short'])
+                    )
+
+        return players_df
+
+    def get_fixtures(self):
+        matches_df = self.get_data('matches')
+        league_entry_df = self.get_data('league_entries')
+
+        # Join to get team names and player names of entry 1 (home team)
+        matches_df = pd.merge(matches_df,
+                            league_entry_df[['id', 'player_first_name']],
+                            how='left',
+                            left_on='league_entry_1',
+                            right_on='id')
+
+        # Join to get team names and player names of entry 2 (away team)
+        matches_df = pd.merge(matches_df,
+                            league_entry_df[['id', 'player_first_name']],
+                            how='left',
+                            left_on='league_entry_2',
+                            right_on='id')
+
+        # Drop unused columns, rename for clearer columns
+        matches_df = (matches_df
+                    .drop(['finished', 'started', 'id_x', 'id_y', 'league_entry_1', 'league_entry_2'], axis=1)
+                    .rename(columns={'event':'match',
+                            'player_first_name_x': 'home_player',
+                            'league_entry_1_points': 'home_score',
+                            'player_first_name_y': 'away_player',
+                            'league_entry_2_points': 'away_score',
+                            })
+                    )
+
         return matches_df
-    
-    elif df_name == 'standings':
-        with open(os.path.realpath("")+'/data/details.json') as json_data:
-            d = json.load(json_data)
-            standings_df = pd.json_normalize(d['standings'])
-            
-        return standings_df
-    
-    # Dataframes from the elements.json
-    elif df_name == 'elements':
-        with open(os.path.realpath("")+'/data/elements.json') as json_data:
-            d = json.load(json_data)
-            elements_df = pd.json_normalize(d['elements'])
-            
-        return elements_df
-    
-    elif df_name == 'element_types':
-        with open(os.path.realpath("")+'/data/elements.json') as json_data:
-            d = json.load(json_data)
-            element_types_df = pd.json_normalize(d['element_types'])
-            
-        return element_types_df
-    
-    # Dataframes from the transactions.json
-    elif df_name == 'transactions':
-        with open(os.path.realpath("")+'/data/transactions.json') as json_data:
-            d = json.load(json_data)
-            transactions_df = pd.json_normalize(d['transactions'])
-            
-        return transactions_df
-    
-    # Dataframes from the element_status.json
-    elif df_name == 'element_status':
-        with open(os.path.realpath("")+'/data/element_status.json') as json_data:
-            d = json.load(json_data)
-            element_status_df = pd.json_normalize(d['element_status'])
-            
-        return element_status_df
 
-def get_team_players():
-    
-    # Pull the required dataframes
-    element_status_df = get_data('element_status')
-    elements_df = get_data('elements')
-    element_types_df = get_data('element_types')
-    league_entry_df = get_data('league_entries')
-    standings_df = get_data('standings')
-    
-    # Built the initial player -> team dataframe
-    players_df = (pd.merge(element_status_df,
-                           league_entry_df,
-                           how="outer",
-                           left_on='owner',
-                           right_on='entry_id'
-                        )
-              .drop(columns=['in_accepted_trade',
-                            'owner',
-                            'status',
-                            'entry_id',
-                            'entry_name',
-                            'id',
-                            'joined_time',
-                            'player_last_name',
-                            'short_name',
-                            'waiver_pick'])
-              .rename(columns={'player_first_name':'team'})
-             )
-    
-    # Get the element details
-    players_df = pd.merge(players_df, elements_df, how="outer", left_on='element', right_on='id')
-    players_df = players_df[['team_x',
-                             'element',
-                             'web_name',
-                             'first_name',
-                             'second_name',
-                             'total_points',
-                             'goals_scored',
-                             'goals_conceded',
-                             'clean_sheets',
-                             'assists',
-                             'bonus',
-                             'draft_rank',
-                             'element_type',
-                             'points_per_game',
-                             'red_cards',
-                             'yellow_cards'
-                            ]]
-    
-    # Get the player types (GK, FWD etc.)
-    players_df = (pd.merge(players_df,
-                         element_types_df,
-                         how='outer',
-                         left_on='element_type',
-                         right_on='id')
-                 .drop(columns=['id',
-                                'plural_name_short',
-                                'singular_name',
-                                'singular_name_short'])
-                )
+    def current_gw(self):
+        matches_df = self.get_data('matches')       
+        num_gameweeks = matches_df[matches_df['finished'] == True]['event'].max()
+        
+        if math.isnan(num_gameweeks):
+            num_gameweeks = 1
 
-    return players_df
-
-def get_fixtures():
-    matches_df = get_data('matches')
-    league_entry_df = get_data('league_entries')
-
-    # Join to get team names and player names of entry 1 (home team)
-    matches_df = pd.merge(matches_df,
-                          league_entry_df[['id', 'player_first_name']],
-                          how='left',
-                          left_on='league_entry_1',
-                          right_on='id')
-
-    # Join to get team names and player names of entry 2 (away team)
-    matches_df = pd.merge(matches_df,
-                          league_entry_df[['id', 'player_first_name']],
-                          how='left',
-                          left_on='league_entry_2',
-                          right_on='id')
-
-    # Drop unused columns, rename for clearer columns
-    matches_df = (matches_df
-                 .drop(['finished', 'started', 'id_x', 'id_y', 'league_entry_1', 'league_entry_2'], axis=1)
-                .rename(columns={'event':'match',
-                           'player_first_name_x': 'home_player',
-                           'league_entry_1_points': 'home_score',
-                           'player_first_name_y': 'away_player',
-                           'league_entry_2_points': 'away_score',
-                          })
-                )
-
-    return matches_df
-
-def current_gw():
-    matches_df = get_data('matches')       
-    num_gameweeks = matches_df[matches_df['finished'] == True]['event'].max()
-    
-    if math.isnan(num_gameweeks):
-        num_gameweeks = 1
-
-    return num_gameweeks
+        return num_gameweeks
