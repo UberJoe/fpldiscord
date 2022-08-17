5a4b0b1047725782021d46037bdddea0414ae2d8
Joe Webster <joe.webster@islonline.com>
2022-08-17 10:45:08 +0100

    attempting new fp

--- draft/teamImg.py
+++ draft/teamImg.py
@@ -50,7 +50,7 @@ class TeamImg:
             background.paste(shirt, offset, shirt)
 
             w, h = shirt.width + shirt.width // 3 * 2, shirt.height // 4
-            textoverlay = self.generate_player_text((w,h), '/app/Helvetica-Bold.ttf', 10, (55,0,60), "White", row.web_name)
+            textoverlay = self.generate_player_text((w,h), '/app/.fonts/fonts/Helvetica-Bold.ttf', 10, (55,0,60), "White", row.web_name)
             textoffset = (player_place - (shirt.width // 4), height + shirt.height)
             background.paste(textoverlay, textoffset, textoverlay)
 
