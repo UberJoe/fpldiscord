e70fd0b283f803ec6e72c99eb8c39856ea02dd78
Joe Webster <joe.webster@islonline.com>
2022-08-17 15:54:54 +0100

    added correct shirts to teamimage command

--- draft/teamImg.py
+++ draft/teamImg.py
@@ -2,12 +2,36 @@ from PIL import Image, ImageDraw, ImageFont, ImageColor
 from cairosvg import svg2png
 import requests
 from io import BytesIO
+import os
 
 class TeamImg:
 
     image_urls = {
         'pitch': 'https://draft.premierleague.com/static/media/pitch-default.dab51b01.svg',
-        'shirt': 'https://draft.premierleague.com/img/shirts/standard/shirt_6-66.png'
+        'shirt': "https://draft.premierleague.com/img/shirts/standard/shirt_{0}-66.png"
+    }
+
+    team_shirt_numbers = {
+        "Arsenal"           :   "3",
+        "Aston Villa"       :   "7",
+        "Bournemouth"       :   "91",
+        "Brentford"         :   "94",
+        "Brighton"          :   "36",
+        "Chelsea"           :   "8",
+        "Crystal Palace"    :   "31",
+        "Everton"           :   "11",
+        "Fulham"            :   "54",
+        "Leeds"             :   "2",
+        "Leicester"         :   "13",
+        "Liverpool"         :   "14",
+        "Man City"          :   "43",
+        "Man Utd"           :   "1",
+        "Newcastle"         :   "4",
+        "Nott'm Forest"     :   "17",
+        "Southampton"       :   "20",
+        "Spurs"             :   "6",
+        "West Ham"          :   "21",
+        "Wolves"            :   "39"
     }
 
     def __init__(self):
@@ -15,18 +39,14 @@ class TeamImg:
         pitch_svg = self.session.get(self.image_urls['pitch'])
 
         pitch_png = BytesIO()
-        svg2png(bytestring=pitch_svg.content, write_to=pitch_png)
+        svg2png(bytestring=pitch_svg.content, write_to=pitch_png, scale=0.44)
         self.background = Image.open(pitch_png)
 
-
-        shirt_png = self.session.get(self.image_urls['shirt'])
-        self.shirt = Image.open(BytesIO(shirt_png.content))
-
     def main(self, team_df):
-        background = self.create_team_image(self.background, self.shirt, team_df)
+        background = self.create_team_image(self.background, team_df)
         return background
 
-    def create_team_image(self, background, overlay, team_df):
+    def create_team_image(self, background, team_df):
         goalkeepers_df = team_df.loc[team_df['plural_name'] == 'Goalkeepers']
         defenders_df = team_df.loc[team_df['plural_name'] == 'Defenders']
         midfielders_df = team_df.loc[team_df['plural_name'] == 'Midfielders']
@@ -35,22 +55,34 @@ class TeamImg:
         row_start = background.height // 12
         row_height = background.height // 5
 
-        background = self.add_player_row(background, overlay, goalkeepers_df, row_start)
-        background = self.add_player_row(background, overlay, defenders_df, row_start + row_height)
-        background = self.add_player_row(background, overlay, midfielders_df, row_start + (row_height*2))
-        background = self.add_player_row(background, overlay, forwards_df, row_start + (row_height*3))
+        background = self.add_player_row(background, goalkeepers_df, row_start)
+        background = self.add_player_row(background, defenders_df, row_start + row_height)
+        background = self.add_player_row(background, midfielders_df, row_start + (row_height*2))
+        background = self.add_player_row(background, forwards_df, row_start + (row_height*3))
 
         return background
 
-    def add_player_row(self, background, shirt, players_df, height):
-        gap, margin = self.calculate_gap(shirt.width, background.width, len(players_df.index))
+    def add_player_row(self, background, players_df, height):
+        shirt_width = 66
+
+        gap, margin = self.calculate_gap(shirt_width, background.width, len(players_df.index))
         player_place = margin
         for row in players_df.itertuples():
+            if row.plural_name == "Goalkeepers":
+                team_num = self.team_shirt_numbers[row.team_name] + "_1"
+            else:
+                team_num = self.team_shirt_numbers[row.team_name]
+                
+            url = self.image_urls['shirt']
+            r = self.session.get(url.format(team_num))
+            b_image = BytesIO(r.content)
+            shirt = Image.open(b_image)
+            
             offset = (player_place, height)
             background.paste(shirt, offset, shirt)
 
             w, h = shirt.width + shirt.width // 3 * 2, shirt.height // 4
-            textoverlay = self.generate_player_text((w,h), '/app/.fonts/fonts/Helvetica-Bold.ttf', 10, (55,0,60), "White", row.web_name)
+            textoverlay = self.generate_player_text((w,h), os.getenv('IMG_FONT'), 12, (55,0,60), "White", row.web_name)
             textoffset = (player_place - (shirt.width // 4), height + shirt.height)
             background.paste(textoverlay, textoffset, textoverlay)
 
